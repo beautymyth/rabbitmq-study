2df6d544fce3590df952f2c161ace6c04190f46e
beautymyth <37394958+beautymyth@users.noreply.github.com>
2018-05-21 16:26:00 +0800

    Add files via upload

---
+++ rpc_fibonacci_client.php
@@ -0,0 +1,97 @@
+<?php
+
+//用于自动加载类中的Lib目录定位
+define('LIB_PATH', __DIR__ . '/Lib/');
+
+//自动加载类
+require_once __DIR__ . '/ComModule/AutoLoader/Loader.php';
+
+//引入连接与消息类
+use Lib\PhpAmqpLib\Connection\AMQPStreamConnection;
+use Lib\PhpAmqpLib\Message\AMQPMessage;
+
+//客户端类
+class FibonacciRpcClient {
+
+    private $connection;
+    private $channel;
+    private $callback_queue;
+    private $response;
+    private $corr_id;
+    private $strQueue = 'queue_rpc_fibonacci';
+
+    public function __construct() {
+        /**
+         * 连接rabbit服务器
+         * host:主机名
+         * port:端口号
+         * user:通过[rabbitmqctl add_user]创建的
+         * password:通过[rabbitmqctl add_user]创建的
+         */
+        $this->connection = new AMQPStreamConnection('127.0.0.1', 5672, 'admin', 'admin');
+
+        //获取或创建一个信道
+        $this->channel = $this->connection->channel();
+
+        /**
+         * 获取一个队列，如果不存在则新建
+         * name:队列名
+         * passive:false
+         * durable:是否持久化的，服务器重启队列不消失
+         * exclusive:是否专有的，允许其他信道访问此队列
+         * auto_delete：是否自动删除，当被消费者连接过，且最后所有消费者都断开连接时
+         */
+        /**
+         * 1.自动生成
+         * 2.专有队列
+         * 3.持久化
+         * 4.自动删除
+         */
+        $arrReturn = $this->channel->queue_declare("", false, false, true, true);
+        $this->callback_queue = $arrReturn[0];
+
+        /**
+         * queue:从哪个队列获取信息
+         * consumer_tag:消费者标识
+         * no_local:不接受此消费者发布的消息
+         * no_ack:服务器是否等待用户确认消息
+         * exclusive:当消费者访问此队列时，是否允许其他消费者访问
+         * nowait:
+         * callback:回调函数
+         */
+        $this->channel->basic_consume($this->callback_queue, '', false, false, false, false, array($this, 'onResponse'));
+    }
+
+    /**
+     * 服务器有返回
+     */
+    public function onResponse($objRep) {
+        //是否响应的自己的请求
+        if ($objRep->get('correlation_id') == $this->corr_id) {
+            $this->response = $objRep->body;
+            //确认请求，从队列删除消息
+            $objRep->delivery_info['channel']->basic_ack($objRep->delivery_info['delivery_tag']);
+        }
+    }
+
+    public function call($intN) {
+        $this->response = null;
+        $this->corr_id = uniqid();
+        $objMsg = new AMQPMessage($intN, array('correlation_id' => $this->corr_id, 'reply_to' => $this->callback_queue));
+        $this->channel->basic_publish($objMsg, '', $this->strQueue);
+
+        //等待服务器返回
+        while (!$this->response) {
+            $this->channel->wait();
+        }
+        return $this->response;
+    }
+
+}
+
+$fibonacci_rpc = new FibonacciRpcClient();
+while (1) {
+    $response = $fibonacci_rpc->call(rand(1, 20));
+    echo " [.] Got ", $response, "\n";
+    //usleep(100 * 1000);
+}
---
+++ rpc_fibonacci_server.php
@@ -0,0 +1,82 @@
+<?php
+
+//用于自动加载类中的Lib目录定位
+define('LIB_PATH', __DIR__ . '/Lib/');
+
+//自动加载类
+require_once __DIR__ . '/ComModule/AutoLoader/Loader.php';
+
+//引入连接与消息类
+use Lib\PhpAmqpLib\Connection\AMQPStreamConnection;
+use Lib\PhpAmqpLib\Message\AMQPMessage;
+
+//队列名
+$strQueue = 'queue_rpc_fibonacci';
+
+/**
+ * 连接rabbit服务器
+ * host:主机名
+ * port:端口号
+ * user:通过[rabbitmqctl add_user]创建的
+ * password:通过[rabbitmqctl add_user]创建的
+ */
+$objConnection = new AMQPStreamConnection('127.0.0.1', 5672, 'admin', 'admin');
+
+//获取或创建一个信道
+$objChannel = $objConnection->channel();
+
+/**
+ * 获取一个队列，如果不存在则新建
+ * name:队列名
+ * passive:是否需要检查已存在同名的队列
+ * durable:是否持久化的，服务器重启队列不消失
+ * exclusive:是否专有的，允许其他信道访问此队列
+ * auto_delete：是否自动删除，当被消费者连接过，且最后所有消费者都断开连接时
+ */
+$objChannel->queue_declare($strQueue, false, true, false, false);
+
+//逻辑运算
+function fib($intN) {
+    if ($intN == 0) {
+        return 0;
+    }
+    if ($intN == 1) {
+        return 1;
+    }
+    return fib($intN - 1) + fib($intN - 2);
+}
+
+//定义回调函数
+function callback_func($objMessage) {
+    $intN = $objMessage->body;
+    echo " [x] fib(", $intN, ")" . PHP_EOL;
+    if ($intN <= 10) {
+        //调用函数进行逻辑运算
+        $intRst = fib($intN);
+        $objReturnMsg = new AMQPMessage($intRst, array('correlation_id' => $objMessage->get('correlation_id')));
+
+        //将计算结果推入客户端预定义的队列
+        $objMessage->delivery_info['channel']->basic_publish($objReturnMsg, '', $objMessage->get('reply_to'));
+        //确认请求，从队列删除消息
+        $objMessage->delivery_info['channel']->basic_ack($objMessage->delivery_info['delivery_tag']);
+    } else {
+        $objReturnMsg = new AMQPMessage($intN . '>10：不做处理', array('correlation_id' => $objMessage->get('correlation_id')));
+        //将计算结果推入客户端预定义的队列
+        $objMessage->delivery_info['channel']->basic_publish($objReturnMsg, '', $objMessage->get('reply_to'));
+        //拒绝请求，将消息重新放入队列
+        $objMessage->delivery_info['channel']->basic_nack($objMessage->delivery_info['delivery_tag'], false, false);
+    }
+}
+
+//每次只获取一条消息
+$objChannel->basic_qos(null, 1, null);
+$objChannel->basic_consume($strQueue, '', false, false, false, false, 'callback_func');
+
+echo " [x] Awaiting RPC requests" . PHP_EOL;
+while (count($objChannel->callbacks)) {
+    $objChannel->wait();
+}
+
+//关闭信道与断开连接
+$objChannel->close();
+$objConnection->close();
