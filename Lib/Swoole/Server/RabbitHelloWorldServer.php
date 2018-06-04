4702771f7be311cd46b2dda445db67eac1b52e57
beautymyth <37394958+beautymyth@users.noreply.github.com>
2018-06-04 19:00:23 +0800

    Update RabbitHelloWorldServer.php

--- Lib/Swoole/Server/RabbitHelloWorldServer.php
+++ Lib/Swoole/Server/RabbitHelloWorldServer.php
@@ -4,7 +4,7 @@ define('LIB_PATH', getenv('InterviewRootPath') . '/Lib/');
 
 include getenv('InterviewRootPath') . 'ComModule/AutoLoader/Loader.php';
 
-use Lib\PhpAmqpLib\Connection\AMQPStreamConnection;
+use Lib\RabbitMQ\RabbitConsumerBase;
 
 /**
  * 消息发送服务端
@@ -27,7 +27,7 @@ class SwooleHelloWorldServer {
         $this->objServer->set(array(
             'daemonize' => 1, //以守护进程执行
             'max_request' => 10000, //worker进程在处理完n次请求后结束运行
-            'worker_num' => 3,
+            'worker_num' => 5,
             "task_ipc_mode " => 3, //使用消息队列通信，并设置为争抢模式,
             'heartbeat_check_interval' => 5, //每隔多少秒检测一次，单位秒，Swoole会轮询所有TCP连接，将超过心跳时间的连接关闭掉
             'heartbeat_idle_time' => 10, //TCP连接的最大闲置时间，单位s , 如果某fd最后一次发包距离现在的时间超过则关闭
@@ -56,22 +56,10 @@ class SwooleHelloWorldServer {
      * 有新的连接进入时
      */
     public function onWorkerStart($server, $worker_id) {
-        $this->strFileName = getenv('InterviewRootPath') . "/logs/rabbit_consume.log";
-        $this->worker_id = $worker_id;
-        $connection = new AMQPStreamConnection('127.0.0.1', 5672, 'admin', 'admin');
-
-        $channel = $connection->channel('2');
-        $channel->queue_declare('hello', false, false, false, false);
-
-        $callback = function($msg) {
-            file_put_contents("{$this->strFileName}", $this->worker_id . '[x] Received ' . $msg->body . '_' . microtime() . "\r\n", FILE_APPEND | LOCK_EX);
-        };
-        $channel->basic_consume('hello', '', false, true, false, false, $callback);
-        while (count($channel->callbacks)) {
-            $channel->wait();
+        $objConsumerTopicNumber = new \Lib\App\ConsumerTopicNumber();
+        if ($objConsumerTopicNumber->init($worker_id)) {
+            $objConsumerTopicNumber->run();
         }
-        $channel->close();
-        $connection->close();
     }
 
     /**
